bdc722feb8f79ee4dc7959273c8b4ddf6291eb38
Mackenzie High <MackenzieHigh2995@GMail.com>
2019-04-28 20:52:58 -0400

    Added Trampoline documentation.

--- src/main/java/com/mackenziehigh/socius/flow/Trampoline.java
+++ src/main/java/com/mackenziehigh/socius/flow/Trampoline.java
@@ -1,33 +1,52 @@
 package com.mackenziehigh.socius.flow;
 
-import com.mackenziehigh.cascade.Cascade;
 import com.mackenziehigh.cascade.Cascade.ActorFactory;
-import com.mackenziehigh.cascade.Cascade.Stage;
 import com.mackenziehigh.cascade.Cascade.Stage.Actor;
 import com.mackenziehigh.cascade.Cascade.Stage.Actor.Context;
 import com.mackenziehigh.cascade.Cascade.Stage.Actor.Input;
 import com.mackenziehigh.cascade.Cascade.Stage.Actor.Output;
-import com.mackenziehigh.socius.io.Printer;
 import java.util.Objects;
 
 /**
+ * A state-machine implementation based on trampolining.
  *
+ * @param <I> is the type of the incoming messages.
+ * @param <O> is the type of the outgoing messages.
  */
 public final class Trampoline<I, O>
         implements DataPipeline<I, O>
 {
+    /**
+     * Signature of a state transition function.
+     *
+     * @param <I> is the type of the incoming messages.
+     * @param <O> is the type of the outgoing messages.
+     */
+    @FunctionalInterface
     public interface State<I, O>
     {
         public State<I, O> onMessage (Context<I, O> context,
                                       I message);
     }
 
+    /**
+     * This is the initial state that the state-machine will be in.
+     */
     private final State<I, O> initial;
 
+    /**
+     * This is the current state that the state-machine is in.
+     */
     private State<I, O> current;
 
+    /**
+     * This actor provides the input connector.
+     */
     private final Actor<I, O> procIn;
 
+    /**
+     * This actor provides the output connector.
+     */
     private final Processor<O> procOut;
 
     private Trampoline (final ActorFactory stage,
@@ -55,75 +74,59 @@ public final class Trampoline<I, O>
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Input<I> dataIn ()
     {
         return procIn.input();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Output<O> dataOut ()
     {
         return procOut.dataOut();
     }
 
+    /**
+     * Get the state that the state-machine is currently in.
+     *
+     * @return the current state.
+     */
     public State<I, O> state ()
     {
         return current;
     }
 
+    /**
+     * Set the state that the state-machine is currently in.
+     *
+     * @param state is the state that the machine will be in.
+     * @return the new state.
+     */
     public State<I, O> state (final State<I, O> state)
     {
-        current = state;
+        current = Objects.requireNonNull(state, "state");
         return state;
     }
 
+    /**
+     * Factory method.
+     *
+     * @param <I> is the type of the incoming messages.
+     * @param <O> is the type of the outgoing messages.
+     * @param stage will be used to create private actors.
+     * @param initial is the state that the machine will be in initially.
+     * @return the new state-machine.
+     */
     public static <I, O> Trampoline<I, O> newTrampolineMachine (final ActorFactory stage,
                                                                 final State<I, O> initial)
     {
         return new Trampoline<>(stage, initial);
     }
 
-    public static void main (String[] args)
-    {
-        final Stage stage = Cascade.newStage();
-        final Trampoline<Integer, String> sm = Trampoline.newTrampolineMachine(stage, Trampoline::state1);
-        final Printer<String> p = Printer.newPrintln(stage, "X = %s");
-        sm.dataOut().connect(p.dataIn());
-
-        for (int i = 0; i < 20; i++)
-        {
-            sm.accept(i);
-        }
-    }
-
-    private static State<Integer, String> state1 (final Context<Integer, String> ctx,
-                                                  final Integer message)
-    {
-        ctx.sendFrom("V" + message);
-
-        if (message < 10)
-        {
-            System.err.println("A = " + message);
-            return Trampoline::state1;
-        }
-        else
-        {
-            return Trampoline::state2;
-        }
-    }
-
-    private static State<Integer, String> state2 (final Context<Integer, String> ctx,
-                                                  final Integer message)
-    {
-        if (message < 100)
-        {
-            System.err.println("B = " + message);
-            return Trampoline::state2;
-        }
-        else
-        {
-            return Trampoline::state1;
-        }
-    }
 }
